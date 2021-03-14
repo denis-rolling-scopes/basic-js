b2f4e78c3fd398e5f3c0da7777e049a753ff08c7
Dzianis Hrynko <d.hrynko@godeltech.com>
2021-03-14 16:27:50 +0300

    feat: solve carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,17 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+  sampleActivity = Number(sampleActivity);
+  if(!sampleActivity || sampleActivity <= 0 || sampleActivity >= 15) {
+    return false;
+  }
+  let activity = parseFloat(sampleActivity);
+  if(!activity) {
+    return false;
+  }
+  return Math.ceil(((Math.log(MODERN_ACTIVITY / activity)) / (Math.log(2)) * HALF_LIFE_PERIOD));
 };
