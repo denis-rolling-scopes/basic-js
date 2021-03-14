80b21c710b395ec5413258b8d0be4f5deeedc822
Dzianis Hrynko <d.hrynko@godeltech.com>
2021-03-14 17:03:10 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let res = '';
+  if(!Array.isArray(members)) {
+    return false;
+  }
+  members = members.filter(function (el) {
+    return typeof el === "string";
+  });
+  members = members.map(function(elem) {
+      return elem.trim().toLocaleUpperCase();
+  })
+  members = members.sort();
+  members.forEach(element => {
+    if(element.length > 0) {
+        res += element.substr(0, 1);
+    }
+  });
+
+  return res;
 };
