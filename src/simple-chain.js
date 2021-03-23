f7681faac16f3acc4cf25677ea690585f4d8d8ca
Dzianis Hrynko <d.hrynko@godeltech.com>
2021-03-23 23:11:35 +0300

    feat: solve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  connector: '~~',
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      this.chain.push(`( ${value} )`);
+      return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(position < 0 || position > this.getLength() || typeof position !== 'number') {
+      this.chain = [];
+      throw new Error;
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let strChain = this.chain.join(this.connector);
+    this.chain = [];
+    return strChain;
   }
 };
 
